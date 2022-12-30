d463ebaff59e652833860f46bae3fd61cfb7efc2
QuibDev <hello@quib.dev>
2022-12-30 12:29:59 +0530

    added curl detection w/ basic ui

--- PoseModule.py
+++ PoseModule.py
@@ -8,6 +8,27 @@ mp_pose = mp.solutions.pose
 # Make detections
 cap = cv2.VideoCapture(0)
 
+# Curl Counter Variable
+curlCount = 0
+stage = None  # down or up
+
+
+
+# angle calculator function
+def calculate_angle(a,b,c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+
+    if (angle > 180.0):
+        angle = 360-angle
+
+    return angle
+
+
 ## Setups mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
@@ -25,6 +46,53 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+        # Extract landmarks
+        landmarks = results.pose_landmarks.landmark
+
+        # Get coordinates
+        leftShoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+        leftElbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+        leftWrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+        # Calculate angle
+        leftBicepAngle = calculate_angle(leftShoulder,leftElbow,leftWrist)
+
+        # Visualize
+        cv2.putText(image, str(leftBicepAngle),
+                    tuple(np.multiply(leftElbow, [640,480]).astype(int)),
+                          cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (255,255,255),
+                          2, cv2.LINE_AA)
+
+
+        # curl coutner logic
+        if leftBicepAngle > 160:
+            stage = "down"
+        if leftBicepAngle < 30 and stage == 'down':
+            stage = "up"
+            curlCount += 1
+            print(curlCount)
+
+        # Render curl counter
+        #setup status box
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+
+        # Rep data
+        cv2.putText(image, 'REPS', (15,12),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2, cv2.LINE_AA)
+
+        cv2.putText(image, str(curlCount),(10,60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Stage data
+        cv2.putText(image, 'STAGE', (65, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+
+        cv2.putText(image, str(stage), (60, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
         # Render detections
         ## draws detections using custom drawing specs
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
@@ -38,6 +106,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
     cap.release()
     cv2.destroyAllWindows()
+
 
 
 
